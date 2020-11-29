37af4702320f015d0c339784a8bfa95e501b5eed
rudradeepeveres <rudradeep.b@everest-ims.com>
2020-11-29 14:36:42 +0530

    added frontend files and db connection

--- backend/src/djreact/settings.py
+++ backend/src/djreact/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'djreact.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'djongoexample',
+        'HOST': 'mongodb+srv://blacklamma:JgZJoAzf2HY5kwRZ@cluster0.jp8le.mongodb.net/djongoexample?retryWrites=true&w=majority',
+        'USER': 'blacklamma',
+        'PASSWORD': 'JgZJoAzf2HY5kwRZ'
     }
 }
 
